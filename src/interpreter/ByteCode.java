fdc6258780c11bc36d6816da805d34e5d7061c53
pprevide <pprevide@yahoo.com>
2019-08-13 14:06:40 -0700

    Added abstract class ByteCode, whose init(), execute(), and print() methods will be implemented by concrete classes

---
+++ src/interpreter/ByteCode.java
@@ -0,0 +1,24 @@
+
+package interpreter;
+
+import java.util.ArrayList;
+
+/**
+ * This is an abstract class for byte code instances, and it contains 
+ * no data fields that relate to arguments, per the instructions.  
+ * The instances of the concrete classes will be responsible for processing
+ * their arguments when ByteCodeLoader executes.
+ * Each method below is implemented by the concrete classes.
+ * Dynamic binding allows the type of each ByteCode to be determined at 
+ * run time.
+ */
+
+public abstract class ByteCode {
+    // Initializes the arguments of the particular bytecode from an ArrayList
+    public abstract void init(ArrayList<String> args);
+    /* Requests that the VM carrry out the instructions of that bytecode
+     * during execution of the program*/
+    public abstract void execute(VirtualMachine vm);
+    // Prints the bytecode appropriately if DUMP is ON
+    public abstract void print(VirtualMachine vm);   
+}
