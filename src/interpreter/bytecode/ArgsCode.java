e31a989adabdfe4679b31d88dbefae5fd80d8fa0
pprevide <pprevide@yahoo.com>
2019-08-29 15:59:59 -0700

    Add ARGS byte code executed by interpreter

---
+++ src/interpreter/bytecode/ArgsCode.java
@@ -0,0 +1,30 @@
+
+package interpreter.bytecode;
+
+import interpreter.ByteCode;
+import interpreter.VirtualMachine;
+
+import java.util.ArrayList;
+
+/**
+ * ARGS byte code: create a new frame starting n elements from the top of the
+ * RunTimeStack.  These n elements are the arguments to be made available in the new
+ * frame, which represents a function call.
+ */
+public class ArgsCode extends ByteCode {
+
+    private int numberArgs;
+
+    public void init(ArrayList<String> args) {
+        numberArgs = Integer.parseInt(args.get(0));
+    }
+
+    public void print(VirtualMachine vm) {
+        System.out.println("ARGS " + numberArgs);
+    }
+
+    public void execute(VirtualMachine vm) {
+        vm.createArgsFrame(numberArgs);
+    }
+
+}
