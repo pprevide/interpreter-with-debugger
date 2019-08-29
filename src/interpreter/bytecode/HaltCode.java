19cdf24d7ef37613c8d1df4b22a39cec249bf781
pprevide <pprevide@yahoo.com>
2019-08-29 16:10:19 -0700

    Reformat code and revise comments to HaltCode class

--- src/interpreter/bytecode/HaltCode.java
+++ src/interpreter/bytecode/HaltCode.java
@@ -7,23 +7,21 @@ import interpreter.VirtualMachine;
 import java.util.ArrayList;
 
 /**
- * HALT byte code: stops execution of program
+ * HALT byte code: stops execution of program.
+ * Upon executing this code, the virtual machine stops running.
  */
-
 public class HaltCode extends ByteCode {
-    
-    public void init(ArrayList<String> args) {}
-    
+
+    public void init(ArrayList<String> args) {
+    }
+
     public void print(VirtualMachine vm) {
         System.out.println("HALT");
     }
-    
+
     public void execute(VirtualMachine vm) {
-        vm.setIsRunning(false); 
+        vm.setIsRunning(false);
     }
-    
-    
-    
-    
-    
+
+
 }
