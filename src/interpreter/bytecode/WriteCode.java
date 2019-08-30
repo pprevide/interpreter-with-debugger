ce372a0a48380b6f1c52d039e66ad78c0411d0fb
pprevide <pprevide@yahoo.com>
2019-08-30 16:26:20 -0700

    Add getter and comments to WriteCode class

--- src/interpreter/bytecode/WriteCode.java
+++ src/interpreter/bytecode/WriteCode.java
@@ -10,17 +10,26 @@ import java.util.ArrayList;
  * WRITE byte code: output the value at top of RunTimeStack to standard output, and return that value.
  */
 public class WriteCode extends ByteCode {
-    
+
     private int value;
-    
-    public void init(ArrayList<String> args) {}
-    
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(int newValue) {
+        this.value = newValue;
+    }
+
+    public void init(ArrayList<String> args) {
+    }
+
     public void print(VirtualMachine vm) {
         System.out.println("WRITE");
     }
-    
+
     public void execute(VirtualMachine vm) {
         value = vm.write();
     }
-    
+
 }
